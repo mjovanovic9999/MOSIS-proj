b322b9cf505235e3d38c06db305102057ca6712c
mjovanovic9999 <73258168+mjovanovic9999@users.noreply.github.com>
2022-07-24 13:09:26 +0200

    usaglaseno posle merge

--- Projekat/StreetsAndTotems/app/src/main/java/mosis/streetsandtotems/MainActivity.kt
+++ Projekat/StreetsAndTotems/app/src/main/java/mosis/streetsandtotems/MainActivity.kt
@@ -2,9 +2,6 @@ package mosis.streetsandtotems
 
 import android.graphics.Color
 import android.os.Bundle
-import android.view.LayoutInflater
-import android.widget.Button
-import android.widget.Toast
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.fillMaxHeight
@@ -28,42 +25,14 @@ import ovh.plrapps.mapcompose.ui.MapUI
 
 @AndroidEntryPoint
 class MainActivity : ComponentActivity() {
-
-
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-
-
         setContent {
             AppTheme {
                 MapContainer()
             }
         }
-
-/*        val requestedepermisions=registerForActivityResult(ActivityResultContracts.RequestMultiplePermissions()){
-            isGranted:Boolean->
-            if (isGranted) Log.i("AAAAAAAA","ok")
-            else Log.i("AAAAAAAA","not ok")
-        }*/
-        reqPermission()
-
-/*        this.registerForActivityResult(
-            ActivityResultContracts.RequestMultiplePermissions()) { permissions ->
-            Log.d("AAAAAAAAA",permissions.toString())
-            permissions.entries.forEach {x->
-                // check whether each permission is granted or not
-                if(x.value==false) Log.i("nece", x.key)
-                else Log.i("ocve",x.key+"KAO OCE")
-            }
-        }*/
-
     }
-
-private fun reqPermission(){
-
-    ActivityCompat.shouldShowRequestPermissionRationale(this,Manifest.permission.ACCESS_COARSE_LOCATION)
-}
-
 }
 
 
@@ -87,106 +56,9 @@ fun MapContainer(
     }
 }
 
-
+@Preview(showBackground = true)
 @Composable
 fun DefaultPreview() {
     AppTheme {
     }
 }
-
-
-
-
-@Composable
-fun MapLayout(){
-    AndroidView(
-        factory = { context: Context ->
-            val view = LayoutInflater.from(context)
-                .inflate(R.layout.map, null, false)
-
-            val btn= view.findViewById<Button>(R.id.button)
-
-            btn.setOnClickListener{
-                Toast.makeText(context,"aaaaaaaa",Toast.LENGTH_SHORT).show()
-            }
-
-
-            getInstance().load(context, androidx.preference.PreferenceManager.getDefaultSharedPreferences(context));
-            val map=view.findViewById<MapView>(R.id.map)
-
-            map.setTileSource(TileSourceFactory.MAPNIK)
-
-            map.setMultiTouchControls(true)
-            map.zoomController.setVisibility(CustomZoomButtonsController.Visibility.NEVER)
-
-
-
-
-
-
-
-
-            val mapController = map.controller
-            mapController.setZoom(context.getString(R.dimen.map_zoom).toDouble())
-
-
-
-
-            val startPoint = GeoPoint( context.getString(R.dimen.nis_latitude).toDouble(),
-                context.getString(R.dimen.nis_longitude).toDouble())
-
-            mapController.setCenter(startPoint)
-
-            // do whatever you want...
-
-            val startMarker = Marker(map)
-            startMarker.position = startPoint
-            startMarker.setAnchor(Marker.ANCHOR_CENTER, Marker.ANCHOR_BOTTOM)
-
-
-            startMarker.icon = AppCompatResources.getDrawable(context,R.drawable.ic_logo_only_tiki)
-            startMarker.title = "Eve ni vammmmm";
-
-
-            map.overlays.add(startMarker)
-
-
-
-
-            val mReceive: MapEventsReceiver = object : MapEventsReceiver {
-                override fun singleTapConfirmedHelper(p: GeoPoint): Boolean {
-                /*    Toast.makeText(
-                        context,
-                        p.latitude.toString() + " - " + p.longitude,
-                        Toast.LENGTH_LONG
-                    ).show()*/
-                    val startMarker = Marker(map)
-                    startMarker.position = GeoPoint(p.latitude,p.longitude)
-                    startMarker.setAnchor(Marker.ANCHOR_CENTER, Marker.ANCHOR_BOTTOM)
-
-
-                    startMarker.icon = AppCompatResources.getDrawable(context,R.drawable.ic_pin_tiki)
-                    startMarker.title = "Eve ni vammmmm";
-                    map.overlays.add(startMarker)
-
-
-                    return false
-                }
-
-                override fun longPressHelper(p: GeoPoint): Boolean {
-                    return false
-                }
-            }
-
-            map.getOverlays().add(MapEventsOverlay(mReceive));
-
-
-            view // return the view
-        },
-        update = { it ->
-
-             }
-    )
-
-}
-
